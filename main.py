a52b4c434fac481868c22a6f20f46acc472ef889
marcus elliott <marcuselliott94@gmail.com>
2024-05-17 14:12:39 +0200

    forgot to delete while true lmao

--- main.py
+++ main.py
@@ -45,17 +45,16 @@ def get_spec_score(profile, spec, season=4):
 async def refresh_scores():
     global report
     async with aiohttp.ClientSession() as session:
-        while True:
-            tasks = []
-            for character in report:
-                tasks.append(asyncio.ensure_future(get_character_profile(session, character['Character'])))
-            profiles = await asyncio.gather(*tasks)
-            print(profiles)
-            for character, profile in zip(report, profiles):
-                if profile:
-                    score = get_spec_score(profile, character['Rolled Spec'])
-                    character['Score'] = score
-            print(json.dumps(report, indent=2))
+        tasks = []
+        for character in report:
+            tasks.append(asyncio.ensure_future(get_character_profile(session, character['Character'])))
+        profiles = await asyncio.gather(*tasks)
+        print(profiles)
+        for character, profile in zip(report, profiles):
+            if profile:
+                score = get_spec_score(profile, character['Rolled Spec'])
+                character['Score'] = score
+        print(json.dumps(report, indent=2))
 
 @app.on_event("startup")
 @repeat_every(seconds=600)
