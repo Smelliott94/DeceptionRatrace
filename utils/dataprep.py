797bbf671325c266d2e3b29f0f7ec1f12e2f5fc7
marcus elliott <marcuselliott94@gmail.com>
2024-07-18 13:06:12 +0200

    drop null char names

--- utils/dataprep.py
+++ utils/dataprep.py
@@ -5,7 +5,9 @@ import json
 
 data = pd.read_csv('../data/rats.csv')
 data = data.iloc[:, :6] # CSV only hass stuff in the first 7 columns
+data = data.dropna(subset=['Challenge Char. Name'])
 data = data.replace({np.nan: None})
+
 print(data)
 with open('../data/rats.json', 'w') as json_file:
     json.dump(data.to_dict(orient='records', ), json_file, indent=2)
